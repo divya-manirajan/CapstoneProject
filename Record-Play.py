37c72635f3ecbb18349a7e4a93795e19db712fd1
divya-manirajan <44423406+divya-manirajan@users.noreply.github.com>
2022-02-05 23:24:57 -0500

    Add files via upload

---
+++ Record-Play.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wav
+
+fs=44100
+duration = 5  # seconds
+myrecording = sd.rec(duration * fs, samplerate=fs, channels=1,dtype='float64')
+print ("Recording Audio")
+sd.wait()
+print ("Audio recording complete , Play Audio")
+sd.play(myrecording, fs)
+sd.wait()
+print ("Play Audio Complete")
+
+
+# In[ ]:
+
+
+
+
